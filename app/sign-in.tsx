7aa05158066b77db7ee5ec87f5e0a9edbe6b7c61
tumon <mofakhar0@gmail.com>
2024-12-29 08:51:17 +0600

    .

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { SafeAreaView, ScrollView, Text, View } from 'react-native'
+import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native'
+import googleImg from "../assets/icons/google.png"
 
 function SignIn() {
   return (
     <SafeAreaView className='h-full'>
       <ScrollView contentContainerClassName="h-full flex justify-center items-center">
         <View>
+          <Text className='text-md text-center font-light text-dark-200 mb-2'> Welcome to Achor </Text>
+          <Text className='text-xl text-center font-bold text-dark'> Let's write whatever you want </Text>
+          <Text className='text-xl text-center font-bold text-primary-300' > Note and Tasks </Text>
+          <Text className='text-md text-center font-light text-dark-200 mt-10' > Login in Achor with Google </Text>
 
-          <Text className='text-lg font-light'> Welcome to Achor </Text>
-          <Text> Let's think and write whatever you want </Text>
-          <Text> Login to Achor with Google </Text>
+          <View className=' flex flex-row justify-center items-center gap-2 ring-4 mt-5 ring-primary-200 rounded-lg p-2'>
+            <Image source={googleImg} className="max-h-8 max-w-8" resizeMode='contain' />
+            {/* NOTE: mt-1 is added as hack */}
+            <Text className='text-base text-center font-bold bg-primary-100 leading-3 mt-1 text-dark' > Continue with Google </Text>
+          </View>
+
         </View>
       </ScrollView>
     </SafeAreaView>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,23 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        100: "#0061FF0A",
+        200: "#0061FF1A",
+        300: "#0061FF",
+      },
+      accent: {
+        100: "#FBFBFD",
+      },
+      dark: {
+        DjkEFAULT: "#000000",
+        100: "#8C8E98",
+        200: "#666876",
+        300: "#191D31",
+      },
+      danger: "#F75555",
+    },
   },
   plugins: [],
 }
