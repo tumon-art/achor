5c26b3e3c6e5024490d033786c1486d424bfac5f
tumon <mofakhar0@gmail.com>
2025-01-02 08:39:02 +0600

    .

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,11 @@
 import { useSignIn } from '@clerk/clerk-expo'
 import { Link, useRouter } from 'expo-router'
-import { Text, TextInput, Button, View, Platform } from 'react-native'
+import { Text, TextInput, View, Platform, TouchableOpacity, Image } from 'react-native'
 import * as WebBrowser from 'expo-web-browser'
 import * as Linking from 'expo-linking'
 import { useOAuth } from '@clerk/clerk-expo'
 import React from 'react'
+import googleImg from "../../assets/icons/google.png"
 
 export const useWarmUpBrowser = () => {
   React.useEffect(() => {
@@ -81,21 +82,36 @@ export default function Page() {
     }
   }, [])
   return (
-    <View>
+    <View className='px-2'>
       <TextInput
         autoCapitalize="none"
         value={emailAddress}
+        className=" p-2 border-none ring-2 my-2 rounded-md"
         placeholder="Enter email"
         onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
       />
       <TextInput
         value={password}
         placeholder="Enter password"
+        className=" p-2 border-none ring-2 my-2 rounded-md"
         secureTextEntry={true}
         onChangeText={(password) => setPassword(password)}
       />
-      <Button title="Sign in" onPress={onSignInPress} />
-      <Button title="Sign in with Google" onPress={onPress} />
+      <TouchableOpacity
+        aria-label='sign-in'
+        className=" p-2 bg-primary-300 text-center
+        hover:opacity-80
+        text-accent-100 rounded-md font-bold my-2"
+        onPress={onSignInPress}> Sign in </TouchableOpacity>
+
+      <TouchableOpacity onPress={onPress}>
+        <View className=' flex flex-row justify-center my-2 items-center bg-primary-100 p-2'>
+          <Image source={googleImg} className="max-h-7 w-auto" resizeMode='contain' />
+          {/* NOTE: mt-1 is added as hack */}
+          <Text className='text-base text-center font-bold
+            bg-primary-100 rounded-md leading-3 mt-1 text-dark' > Continue with Google </Text>
+        </View>
+      </TouchableOpacity>
       <View>
         <Text>Don't have an account?</Text>
         <Link href="./sign-up">
